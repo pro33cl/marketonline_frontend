4d8b1831f9242176943cf7090a15650d6186f01a
pro33cl <pro33cl@yahoo.com>
2024-05-20 15:19:09 -0400

    Frontend Avance 2024 05 20 15.19

--- src/views/PagePurchaseResult/PagePurchaseResult.jsx
+++ src/views/PagePurchaseResult/PagePurchaseResult.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function PagePurchaseResult() {
   return (
-    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", padding: "1rem", width: "100%"}}>
       <Spinner animation="border" role="status"></Spinner>
       <div className='fs-4 fw-bold mt-3'>Redirigiendo a medio de pago ...</div>
     </div>
